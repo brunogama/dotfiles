dd6d1a3a2686308900913bfaacc2bda64b52b28f
Bruno Gama <bgamap@gmail.com>
2014-11-23 00:39:25 -0200

    Updated Exports

--- exports.zsh
+++ exports.zsh
@@ -2,12 +2,22 @@
 
 export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
 export ANDROID_HOME=/Applications/android-sdk
-export PATH=$PATH:/usr/local/bin:/usr/local/sbin:$HOMEBREW_HOME/bin:$HOMEBREW_HOME/sbin:$_BGDOTFILES/bin:${DEVELOPER_DIR}/usr/bin:$ANDROID_HOME:/usr/bin
+# export PATH=$PATH:/usr/local/bin:/usr/local/sbin:$HOMEBREW_HOME/bin:$HOMEBREW_HOME/sbin:$_BGDOTFILES/bin:${DEVELOPER_DIR}/usr/bin:$ANDROID_HOME:/usr/bin
+path=(
+    /usr/local/{bin:,sbin}
+    $HOMEBREW_HOME/{bin,sbin}
+    $_BGDOTFILES/bin
+    ${DEVELOPER_DIR}/usr/bin
+    $ANDROID_HOME:/usr/bin
+    $path
+)
 export GRADLE_OPTS='-Dorg.gradle.daemon=true'
-export EDITOR='subl'
+export EDITOR=`which subl`
 export GIT_EDITOR='vim'
 
 [ -d '/opt/local/share/man' ] && export MANPATH=/opt/local/share/man:$MANPATH
 [ -d '/Library/Java/Home' ] && export JAVA_HOME=/Library/Java/Home
 [ hash node 2>/dev/null ] && export NODE_PATH=`which node`
-[ hash rvm 2>/dev/null ] && export PATH=$PATH:$HOME/.rvm/bin
+
+export DEFAULT_USER='bruno'
+export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
